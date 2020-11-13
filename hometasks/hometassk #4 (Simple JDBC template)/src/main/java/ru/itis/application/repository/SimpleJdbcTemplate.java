2b89b13773a19fe45aab09fd4ce19a3b83178bb9
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-11-13 19:47:13 +0300

    added execute method

--- hometasks/hometassk #4 (Simple JDBC template)/src/main/java/ru/itis/application/repository/SimpleJdbcTemplate.java
+++ hometasks/hometassk #4 (Simple JDBC template)/src/main/java/ru/itis/application/repository/SimpleJdbcTemplate.java
@@ -1,5 +1,6 @@
 package ru.itis.application.repository;
 
+import javax.print.attribute.standard.PresentationDirection;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ public class SimpleJdbcTemplate {
         this.dataSource = dataSource;
     }
 
+    // на select и пр.
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object ... args) {
         ResultSet resultSet = null;
         Connection connection = null;
@@ -47,6 +49,35 @@ public class SimpleJdbcTemplate {
             if (resultSet != null) {
                 try {
                     resultSet.close();
+                } catch (SQLException ex) {
+                }
+            }
+        }
+    }
+
+    // на update, insert и пр
+    public <T> int execute(String sql, Object ... args) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                statement.setObject(i + 1, args[i]);
+            }
+            return statement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new IllegalStateException(ex);
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
                 } catch (SQLException ex) {
                 }
             }
