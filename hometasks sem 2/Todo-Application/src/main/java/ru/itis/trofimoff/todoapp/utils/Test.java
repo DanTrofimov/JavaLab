70601df4fd4235c83d3a080033b9a2e014faee40
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-03-15 15:02:49 +0300

    pagination

---
+++ hometasks sem 2/Todo-Application/src/main/java/ru/itis/trofimoff/todoapp/utils/Test.java
@@ -0,0 +1,39 @@
+package ru.itis.trofimoff.todoapp.utils;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    private static final String endpoint = "https://api.telegram.org/bot1614707727:AAGCxX_J_zCxdJ65mDpkozSxq5lX0Po5JJc/sendDocument?chat_id=406039741";
+    private static final String filePath = "D:\\images\\cat.jpg";
+    public static void main(String[] args) throws IOException {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(URI.create(endpoint));
+
+        MultipartEntity reqEntity = new MultipartEntity();
+        reqEntity.addPart("document", new FileBody(new File(filePath)));
+
+        httppost.setEntity(reqEntity);
+
+        HttpResponse response = httpclient.execute(httppost);
+
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+}
--- hometasks sem 2/Todo-Application/src/main/webapp/WEB-INF/freemarker/views/main.ftl
+++ hometasks sem 2/Todo-Application/src/main/webapp/WEB-INF/freemarker/views/main.ftl
@@ -49,14 +49,16 @@
         </div>
         <div class="todo-list">
             <#if todos?has_content>
-                <#list todos as todo>
-                    <@userTodo.todo text="${todo.getText()}" id="${todo.getId()}" todoGroup="${todo.getGroup().getId()}"/>
-                </#list>
-                <form class="todo-form" action="add-todo" method="post">
-                    <input type="text" name="todoText" placeholder="<@spring.message "main_page.task.placeholder"/>">
-                    <button type="submit"><@spring.message "main_page.task.add"/></button>
-                </form>
-                
+                <div>
+                    <#list todos as todo>
+                        <@userTodo.todo text="${todo.getText()}" id="${todo.getId()}" todoGroup="${todo.getGroup().getId()}"/>
+                    </#list>
+                    <form class="todo-form" action="add-todo" method="post">
+                        <input type="text" name="todoText" placeholder="<@spring.message "main_page.task.placeholder"/>">
+                        <button type="submit"><@spring.message "main_page.task.add"/></button>
+                    </form>
+                </div>
+
                 <#if pageAmount?? && size??>
                     <div class="pagination">
                         <#if currentPage gt 0>
